6a884f3bd641cc9569083187ba054bd0c58685c9
Mawer <mawermeow@protonmail.com>
2022-06-08 13:12:06 +0800

    改變按鈕佈局

--- components/editor/ControlButton.js
+++ components/editor/ControlButton.js
@@ -6,21 +6,18 @@ const ControlButton = (props) => {
 
 
     return <div className={classes.controlButton}>
-        <span className={showFutureLog ? classes.unControlButton:''}>
-            <button onClick={() => props.onToggleFutureLog()}>
-                {/*{showFutureLog ? '隱藏' : '顯示'}*/}
-                未來筆記
-            </button>
-        </span>
         <span className={showPastLog ? classes.unControlButton:''}>
             <button onClick={() => props.onTogglePastLog()}>
-                {/*{showFutureLog ? '隱藏' : '顯示'}*/}
                 過去筆記
+            </button>
+        </span>
+        <span className={showFutureLog ? classes.unControlButton:''}>
+            <button onClick={() => props.onToggleFutureLog()}>
+                未來筆記
             </button>
         </span>
         <span className={showTaskLog ? classes.unControlButton:''}>
             <button onClick={() => props.onToggleTaskLog()}>
-                {/*{showTaskLog ? '所有筆記項目' : '待辦事項清單'}*/}
                 僅任務
             </button>
         </span>
